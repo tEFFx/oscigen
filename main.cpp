4b241dc359675f6a8f64778cc2c2b95b2945bacc
tEFFx <erikkoii@gmail.com>
2017-06-05 21:02:05 +0200

    Cleaned up code & added preview window

--- main.cpp
+++ main.cpp
@@ -11,42 +11,54 @@
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
 
-void drawWaveform(sf::RenderTarget& target, sf::SoundBuffer& buffer, int playbackPos, const char index, const char count, const int length);
-short averageSample(const short* samples, const unsigned int startIndex, const unsigned char count);
-int renderRealtime(const std::vector<sf::SoundBuffer*>& buffers, const short bufSize);
-int renderImages(const std::vector<sf::SoundBuffer*>& buffers, const short bufSize, const char* masterAudio);
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+
+void drawWaveform(sf::RenderTarget& target, sf::SoundBuffer& buffer, int playbackPos, const uint8 index, const uint8 count, const uint32 length);
+short averageSample(const short* samples, const uint32 startIndex, const uint8 count);
 
 int main(int argc, char* argv[])
 {
-	int mode = 0;
+	bool preview = true;
 
-	std::string outputFile = "output";
+	std::string outputFile = "output.mp4";
 	std::vector<std::string> inputFiles;
 
 	std::string prevCmd = "";
-	for(unsigned char i = 0; i < argc; i++) {
+	for(uint8 i = 1; i < argc; i++) {
 		std::string cmd = std::string(argv[i]);
 
-		if(cmd[0] != '-') {
+		if(prevCmd != "" && cmd[0] != '-') {
 			if(prevCmd == "-i") {
-				inputFiles.push_back(std::string(argv[i]));
+				std::string input = std::string(argv[i]);
+				inputFiles.push_back(input);
+				std::cout << "Input:" << input << std::endl;
 			} else if(prevCmd == "-o") {
 				outputFile = std::string(argv[i]);
+				std::cout << "Output: " << outputFile << std::endl;
 			}
 		} else {
 			if(cmd == "-i" || cmd == "-o") {
 				prevCmd = std::string(argv[i]);
-			} else if(cmd == "-r") {
-				mode = 1;
-			}
-			else if(cmd == "-?") {
-				std::cout << "HALP" << std::endl;
+			} else if(cmd == "-h") {
+				preview = false;
+			} else if(cmd == "-?") {
+				std::cout 	<< "Oscigen commands" << std::endl << std::endl
+							<< "-i\tSpecifies audio files used. First is master audio." << std::endl
+							<< "-o\tOutput file. Default is \"output.mp4\"." << std::endl
+							<< "-h\tHeadless mode, disables preview window." << std::endl
+							<< "-?\tPrints all available commands." << std::endl;
+				return -1;
+			} else {
+				std::cout << cmd << " is not a recognised command! Use -? for list of available commands." << std::endl;
+				return -1;
 			}
 		}
 	}
 
 	std::vector<sf::SoundBuffer*> buffers;
-	for (unsigned char i = 0; i < inputFiles.size(); i++) {
+	for (uint8 i = 0; i < inputFiles.size(); i++) {
 		sf::SoundBuffer* buffer = new sf::SoundBuffer();
 		if(buffer->loadFromFile(inputFiles[i])) {
 			buffers.push_back(buffer);
@@ -58,22 +70,84 @@ int main(int argc, char* argv[])
 	int bufSize = buffers.size();
 	if(bufSize == 0) {
 		std::cout << "ERROR: No valid input file(s) specified!" << std::endl;
-		return 0;
+		return -1;
 	}
 
-	switch(mode) {
-		case 0:
-			renderImages(buffers, bufSize, inputFiles[0].c_str());
-			break;
-		case 1:
-			renderRealtime(buffers, bufSize);
-			break;
+	sf::RenderTexture target;
+	target.create(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	sf::RenderWindow* window = NULL;
+	sf::Sprite* previewSpr = NULL;
+	if(preview) {
+		window = new sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Rendering...");
+		previewSpr = new sf::Sprite(target.getTexture());
 	}
+
+	std::ostringstream cmd;
+	cmd << 	"ffmpeg -r 60 -f rawvideo -pix_fmt rgba -s 1280x720 -i - -i " << inputFiles[0] << " -strict -2 " <<
+			"-threads 0 -preset fast -y -pix_fmt yuv420p -crf 21 " << outputFile;
+	FILE* ffmpeg = popen(cmd.str().c_str(), "w");
+
+	const uint16 samplesPerFrame = buffers[0]->getSampleRate() / 60;
+	const uint32 numFrames = buffers[0]->getSampleCount() / buffers[0]->getChannelCount() / samplesPerFrame;
+	std::cout << "Preparing to render " << numFrames << " frames" << std::endl;
+
+	uint32 currentFrame = 0;
+	while(currentFrame < numFrames) {
+		uint32 sampleIndex = currentFrame * samplesPerFrame;
+
+		target.clear();
+
+		if(bufSize > 1) {
+			for (uint8 i = 0; i < bufSize - 1; i++) {
+				drawWaveform(target, *buffers[i + 1], sampleIndex, i, bufSize - 1, samplesPerFrame);
+			}
+		} else {
+			drawWaveform(target, *buffers[0], sampleIndex, 0, 1, samplesPerFrame);
+		}
+
+		target.display();
+
+		sf::Image img = target.getTexture().copyToImage();
+		fwrite(img.getPixelsPtr(), WINDOW_WIDTH*WINDOW_HEIGHT*4, 1, ffmpeg);
+
+		currentFrame++;
+
+		if(window != NULL){
+			if(!window->isOpen()) {
+				delete(window);
+				delete(previewSpr);
+				window = NULL;
+				previewSpr = NULL;
+				break;
+			}
+
+			sf::Event event;
+			while(window->pollEvent(event)){
+				if(event.type == sf::Event::Closed)
+					window->close();
+			}
+
+			window->clear();
+			window->draw(*previewSpr);
+			window->display();
+
+			int progress = ceil(((float)currentFrame / (float)numFrames) * 10000) / 100;
+			std::ostringstream title;
+			title << "Rendering... " << progress << "%";
+			window->setTitle(title.str());
+		}
+	}
+
+	pclose(ffmpeg);
+
+	if(window != NULL)
+		window->close();
 
 	return 0;
 }
 
-void drawWaveform(sf::RenderTarget& target, sf::SoundBuffer& buffer, int playbackPos, const char index, const char count, const int length) {
+void drawWaveform(sf::RenderTarget& target, sf::SoundBuffer& buffer, int playbackPos, const uint8 index, const uint8 count, const uint32 length) {
 	float interval = WINDOW_WIDTH / (float)length;
 	float waveHeight = WINDOW_HEIGHT * (1.0f / count);
 	float invAmplitude = (1.0f / SHRT_MAX) * waveHeight;
@@ -81,13 +155,18 @@ void drawWaveform(sf::RenderTarget& target, sf::SoundBuffer& buffer, int playbac
 	if(count > 1)
 		verticalCenter -= waveHeight * 0.25f;
 
-	const unsigned int channels = buffer.getChannelCount();
+	const uint32 numSamples = buffer.getSampleCount();
+	const uint32 channels = buffer.getChannelCount();
 	const short* samples = buffer.getSamples();
 	playbackPos *= channels;
 
-	unsigned short centerSnap = 0;
-    unsigned short maxAmplitude = 0;
-	for (unsigned short i = 0; i < length * 2; i += channels) {
+	uint16 centerSnap = 0;
+    uint16 maxAmplitude = 0;
+	for (uint16 i = 0; i < length * 2; i += channels) {
+		uint32 samplePos = playbackPos + length + i;
+		if(samplePos >= numSamples)
+			break;
+
 		short sample = averageSample(samples, playbackPos + i + length, channels);
 
 		if(sample >= maxAmplitude) {
@@ -97,12 +176,13 @@ void drawWaveform(sf::RenderTarget& target, sf::SoundBuffer& buffer, int playbac
 	}
 
 	playbackPos += centerSnap;
-	if(centerSnap < 0)
-		centerSnap = 0;
 
 	sf::Vertex vertices[length];
-	for (unsigned short i = 0; i < length; i++) {
-		short sample = averageSample(samples, playbackPos + i * channels, channels);
+	for (uint16 i = 0; i < length; i++) {
+		short sample = 0;
+		uint32 samplePos = playbackPos + i * channels;
+		if(samplePos < numSamples)
+			sample = averageSample(samples, playbackPos + i * channels, channels);
 
 		vertices[i] = sf::Vector2f(i * interval, verticalCenter + sample * invAmplitude);
 	}
@@ -110,83 +190,11 @@ void drawWaveform(sf::RenderTarget& target, sf::SoundBuffer& buffer, int playbac
 	target.draw(vertices, length, sf::LinesStrip);
 }
 
-short averageSample(const short* samples, const unsigned int startIndex, const unsigned char count) {
+short averageSample(const short* samples, const uint32 startIndex, const uint8 count) {
 	short result = 0;
-	for (unsigned int i = startIndex; i < startIndex + count; i++) {
+	for (uint32 i = startIndex; i < startIndex + count; i++) {
 		result += samples[i];
 	}
 
 	return result / count;
-}
-
-int renderRealtime(const std::vector<sf::SoundBuffer*>& buffers, const short bufSize) {
-		sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Oscigen");
-
-		sf::Sound bgMusic;
-		bgMusic.setBuffer(*buffers[0]);
-		bgMusic.play();
-
-		while (window.isOpen()) {
-			sf::Event event;
-			while (window.pollEvent(event))	{
-			    if (event.type == sf::Event::Closed)
-					window.close();
-			}
-
-			window.clear();
-
-			int playbackPos = bgMusic.getBuffer()->getSampleRate() * bgMusic.getPlayingOffset().asSeconds();
-			if(bufSize > 1) {
-				for (unsigned char i = 0; i < bufSize - 1; i++) {
-					drawWaveform(window, *buffers[i + 1], playbackPos, i, bufSize - 1, SAMPLES_PER_FRAME);
-				}
-			} else {
-				drawWaveform(window, *buffers[0], playbackPos, 0, 1, SAMPLES_PER_FRAME);
-			}
-			window.display();
-		}
-
-		bgMusic.stop();
-
-		return 0;
-}
-
-int renderImages(const std::vector<sf::SoundBuffer*>& buffers, const short bufSize, const char* masterAudio) {
-	std::ostringstream cmd;
-	cmd << 	"ffmpeg -r 60 -f rawvideo -pix_fmt rgba -s 1280x720 -i - -i " << masterAudio << " -strict -2 " <<
-			"-threads 0 -preset fast -y -pix_fmt yuv420p -crf 21 output.mp4";
-	FILE* ffmpeg = popen(cmd.str().c_str(), "w");
-
-	const unsigned short samplesPerFrame = buffers[0]->getSampleRate() / 60;
-	const unsigned int numFrames = (buffers[0]->getSampleCount()) / samplesPerFrame;
-	std::cout << "Preparing to render " << numFrames << " frames" << std::endl;
-
-	sf::RenderTexture frame;
-	frame.create(WINDOW_WIDTH, WINDOW_HEIGHT);
-
-	unsigned int currentFrame = 0;
-	while(currentFrame < numFrames) {
-		unsigned int sampleIndex = currentFrame * samplesPerFrame;
-
-		frame.clear();
-
-		if(bufSize > 1) {
-			for (unsigned char i = 0; i < bufSize - 1; i++) {
-				drawWaveform(frame, *buffers[i + 1], sampleIndex, i, bufSize - 1, samplesPerFrame);
-			}
-		} else {
-			drawWaveform(frame, *buffers[0], sampleIndex, 0, 1, samplesPerFrame);
-		}
-
-		frame.display();
-
-		sf::Image img = frame.getTexture().copyToImage();
-		fwrite(img.getPixelsPtr(), WINDOW_WIDTH*WINDOW_HEIGHT*4, 1, ffmpeg);
-
-		currentFrame++;
-	}
-
-	pclose(ffmpeg);
-
-	return 0;
 }
